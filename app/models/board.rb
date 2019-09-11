2d0419ceb287bdf70847bcb2109fd123986b615b
Douglas Uretsky <douglasuretsky@gmail.com>
2019-09-11 10:10:30 -0400

    add full? and draw? methods to Board class

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,8 +1,32 @@
 class Board < ApplicationRecord
     serialize :grid, Array
     attr_accessor :grid
+    
+    
+    WIN_COMBINATIONS = [
+        [0,1,2], # Top row
+        [3,4,5],  # Middle row
+        [6,7,8], # Bottom row
+        [0,4,8], # Diag. right to left
+        [6,4,2], # Diag. left to right
+        [0,3,6], # Left vertical
+        [7,4,1], # Mid vertical
+        [2,5,8] # Right vertical
+    ]
 
     def start_game
         self.grid = Array.new(9) { "" }
+    end
+
+    def full?
+        self.grid.all? { |pos| pos == "X" || pos == "O" }
+      end
+      
+    def draw?
+        if full? && !won?
+            return true
+        else
+            return false
+        end
     end
 end
