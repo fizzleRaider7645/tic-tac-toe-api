7caf958ab250260986a11c59c530bc8776a10e92
Douglas Uretsky <douglasuretsky@gmail.com>
2019-09-12 07:26:29 -0400

    drop Boards table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_170745) do
-
-  create_table "boards", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.text "grid"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
