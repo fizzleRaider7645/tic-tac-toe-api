3e3e8d5832c06a4954d00c88e6cd3247d0aabac5
Douglas Uretsky <douglasuretsky@gmail.com>
2019-09-12 11:23:31 -0400

    create controller/model/table for Game

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_09_12_152151) do
+
+  create_table "games", force: :cascade do |t|
+    t.text "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
