39fbd57edfc87a2061d634de960abf98b84af7e3
Douglas Uretsky <douglasuretsky@gmail.com>
2019-09-13 13:14:28 -0400

    create Board component

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React,  { Component } from 'react';
+import Board from './components/Board';
 import './App.css';
 
 class App extends Component {
---
+++ client/src/components/Board.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+class Board extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div>
+                Board
+            </div>
+        )
+    }
+}
+
+export default Board
