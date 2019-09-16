719c1fd46927b56751336c8e623e4efdc2b0bea1
Douglas Uretsky <douglasuretsky@gmail.com>
2019-09-16 11:02:39 -0400

    create positionTaken in gameLogic module

---
+++ client/modules/gameLogic.js
@@ -0,0 +1,14 @@
+function positionTaken(board, index)
+  !(board[index] == " ")
+end
+
+const WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
