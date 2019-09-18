8d472d9e999934d376aee6ff5334dc3adf6c3fbe
Douglas Uretsky <douglasuretsky@gmail.com>
2019-09-18 10:31:20 -0400

    add index action to GameController

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,2 +1,18 @@
 class GameController < ApplicationController
+
+    def index
+        games = Game.all
+        render json: games
+    end
+
+
+    private
+
+    def game_params
+      params.permit(state: [])
+    end
+  
+    def set_game
+      @game = Game.find(params[:id])
+    end
 end
