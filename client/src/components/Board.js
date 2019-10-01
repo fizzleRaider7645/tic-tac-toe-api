e1d551f67d1327e2e8395c442363a1b84a36a285
Douglas Uretsky <douglasuretsky@gmail.com>
2019-10-01 16:56:26 -0400

    refactor Board to be functional component

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -1,30 +1,49 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+// import React, { Component } from 'react';
+import React from 'react';
 import Cell from './Cell'
+import { connect } from 'react-redux';
 import { placeToken } from '../actions/BoardActions';
 
-class Board extends Component {
-    constructor() {
-        super()
-    }
+// class Board extends Component {
+//     constructor() {
+//         super()
+//     }
 
-    render() {
-        return (
-            <div className="board">
-                <Cell id={0} value={this.props.game.board ? this.props.game.board[0] : ""} placeToken={this.props.placeToken}/>
-                <Cell id={1} value={this.props.game.board ? this.props.game.board[1] : ""} placeToken={this.props.placeToken}/>
-                <Cell id={2} value={this.props.game.board ? this.props.game.board[2] : ""} placeToken={this.props.placeToken}/>
+//     render() {
+//         return (
+//             <div className="board">
+//                 <Cell id={0} value={props.game.board ? props.game.board[0] : ""} placeToken={props.placeToken}/>
+//                 <Cell id={1} value={props.game.board ? props.game.board[1] : ""} placeToken={props.placeToken}/>
+//                 <Cell id={2} value={props.game.board ? props.game.board[2] : ""} placeToken={props.placeToken}/>
 
-                <Cell id={3} value={this.props.game.board ? this.props.game.board[3] : ""} placeToken={this.props.placeToken}/>
-                <Cell id={4} value={this.props.game.board ? this.props.game.board[4] : ""} placeToken={this.props.placeToken}/>
-                <Cell id={5} value={this.props.game.board ? this.props.game.board[5] : ""} placeToken={this.props.placeToken}/>
+//                 <Cell id={3} value={props.game.board ? props.game.board[3] : ""} placeToken={props.placeToken}/>
+//                 <Cell id={4} value={props.game.board ? props.game.board[4] : ""} placeToken={props.placeToken}/>
+//                 <Cell id={5} value={props.game.board ? props.game.board[5] : ""} placeToken={props.placeToken}/>
 
-                <Cell id={6} value={this.props.game.board ? this.props.game.board[6] : ""} placeToken={this.props.placeToken}/>
-                <Cell id={7} value={this.props.game.board ? this.props.game.board[7] : ""} placeToken={this.props.placeToken}/>
-                <Cell id={8} value={this.props.game.board ? this.props.game.board[8] : ""} placeToken={this.props.placeToken}/>
-            </div>
-        )
-    }
+//                 <Cell id={6} value={props.game.board ? props.game.board[6] : ""} placeToken={props.placeToken}/>
+//                 <Cell id={7} value={props.game.board ? props.game.board[7] : ""} placeToken={props.placeToken}/>
+//                 <Cell id={8} value={props.game.board ? props.game.board[8] : ""} placeToken={props.placeToken}/>
+//             </div>
+//         )
+//     }
+// }
+
+const Board = props => {
+    return (
+        <div className="board">
+            <Cell id={0} value={props.game.board ? props.game.board[0] : ""} placeToken={props.placeToken}/>
+            <Cell id={1} value={props.game.board ? props.game.board[1] : ""} placeToken={props.placeToken}/>
+            <Cell id={2} value={props.game.board ? props.game.board[2] : ""} placeToken={props.placeToken}/>
+        
+            <Cell id={3} value={props.game.board ? props.game.board[3] : ""} placeToken={props.placeToken}/>
+            <Cell id={4} value={props.game.board ? props.game.board[4] : ""} placeToken={props.placeToken}/>
+            <Cell id={5} value={props.game.board ? props.game.board[5] : ""} placeToken={props.placeToken}/>
+        
+            <Cell id={6} value={props.game.board ? props.game.board[6] : ""} placeToken={props.placeToken}/>
+            <Cell id={7} value={props.game.board ? props.game.board[7] : ""} placeToken={props.placeToken}/>
+            <Cell id={8} value={props.game.board ? props.game.board[8] : ""} placeToken={props.placeToken}/>
+        </div>
+    )
 }
 
 export default connect(null, { placeToken })(Board)
