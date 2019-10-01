e96e89d1b3ca84445b1f52cc957fab75ab699282
Douglas Uretsky <douglasuretsky@gmail.com>
2019-10-01 16:52:24 -0400

    refactor GameMenu to be stateless

--- client/src/components/GameMenu.js
+++ client/src/components/GameMenu.js
@@ -1,19 +1,30 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+// import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+import React from 'react'
 
-class GameMenu extends Component {
-    constructor(props) {
-        super(props)
-    }
+// class GameMenu extends Component {
+//     constructor(props) {
+//         super(props)
+//     }
     
-    render() {
-        return (
-            <div id="game-menu">
-                <p onClick={this.props.saveGameClick}>Save</p>
-                <p onClick={this.props.endGame}>Exit</p>
-            </div>
-        )
-    }
+//     render() {
+//         return (
+//             <div id="game-menu">
+//                 <p onClick={this.props.saveGameClick}>Save</p>
+//                 <p onClick={this.props.endGame}>Exit</p>
+//             </div>
+//         )
+//     }
+// }
+
+const GameMenu = props => {
+    return (
+        <div id="game-menu">
+            <p onClick={props.saveGameClick}>Save</p>
+            <p onClick={props.endGame}>Exit</p>
+        </div>
+    )
 }
 
-export default connect(null, { })(GameMenu);
+// export default connect(null, { })(GameMenu);
+export default GameMenu
