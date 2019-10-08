0a06415a6ba9c46efc43aaaf277ec43a9a927dab
Douglas Uretsky <douglasuretsky@gmail.com>
2019-10-08 09:22:36 -0400

    create draw scroll

--- client/src/components/WinnerScroll.js
+++ client/src/components/WinnerScroll.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const WinnerScroll = props => {
-    
+    return (
+        <div id="winner-scroll">
+            {props} Won!
+        </div>
+    )
 }
 
 export default WinnerScroll
