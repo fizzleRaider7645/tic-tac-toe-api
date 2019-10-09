36fa1d733d7a22929faf2ecfeffd02a767459f90
Douglas Uretsky <douglasuretsky@gmail.com>
2019-10-09 16:39:56 -0400

    remove constructor from Board componenet

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -1,32 +1,7 @@
-// import React, { Component } from 'react';
 import React from 'react';
 import Cell from './Cell'
 import { connect } from 'react-redux';
 import { placeToken } from '../actions/BoardActions';
-
-// class Board extends Component {
-//     constructor() {
-//         super()
-//     }
-
-//     render() {
-//         return (
-//             <div className="board">
-//                 <Cell id={0} value={props.game.board ? props.game.board[0] : ""} placeToken={props.placeToken}/>
-//                 <Cell id={1} value={props.game.board ? props.game.board[1] : ""} placeToken={props.placeToken}/>
-//                 <Cell id={2} value={props.game.board ? props.game.board[2] : ""} placeToken={props.placeToken}/>
-
-//                 <Cell id={3} value={props.game.board ? props.game.board[3] : ""} placeToken={props.placeToken}/>
-//                 <Cell id={4} value={props.game.board ? props.game.board[4] : ""} placeToken={props.placeToken}/>
-//                 <Cell id={5} value={props.game.board ? props.game.board[5] : ""} placeToken={props.placeToken}/>
-
-//                 <Cell id={6} value={props.game.board ? props.game.board[6] : ""} placeToken={props.placeToken}/>
-//                 <Cell id={7} value={props.game.board ? props.game.board[7] : ""} placeToken={props.placeToken}/>
-//                 <Cell id={8} value={props.game.board ? props.game.board[8] : ""} placeToken={props.placeToken}/>
-//             </div>
-//         )
-//     }
-// }
 
 const Board = props => {
     return (
